46bd73a85498ba1c33670a9151030e345c55d753
DESKTOP-7CI0M2E\INF <monique@infinitum.co.kr>
2022-08-16 14:52:59 +0900

    first commit

---
+++ src/main/java/com/infinitum/monique/MoniqueApplication.java
@@ -0,0 +1,23 @@
+package com.infinitum.monique;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.infinitum.monique.mapper")
+public class MoniqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoniqueApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+}
---
+++ src/test/java/com/infinitum/monique/MoniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinitum.monique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoniqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
